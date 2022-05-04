e5b46d1a1d07528f5b2dd0d3582730cde2c08d28
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-05-04 18:13:07 +0200

    gg

--- analisis.py
+++ analisis.py
@@ -3,5 +3,18 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-file=pd.read_csv("USA_Housing",sep=",")
-data=pd.DataFrame(file)
+file=pd.read_csv("USA_Housing.csv",sep=",")
+
+print(file.head(),
+file.columns,
+file.describe())
+
+for columna in file.columns:
+    if columna!="Address":
+        sns.boxplot(x=file[columna])
+        plt.show()
+    else:
+        sns.histplot(x=file[columna])
+
+file=file.dropna().head
+
